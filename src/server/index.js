427f2922a3ab6c02a308ec95373d370d1a845a15
Bayoumi-dev <bayoumi.c2@gmail.com>
2022-01-02 14:11:13 +0200

    config prod mode

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 const mockAPI = require("./mockAPI.js");
 const handleDataRequest = require("./dataRequest.js");
 
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const config = require("../../webpack.prod");
+
 // Start up an instance of app
 const app = express();
 
@@ -14,6 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Cors for cross origin allowance
 app.use(cors());
+
+// Initialize the static main file
+// app.use(express.static("dist"));
+
+// Express use the webpack-dev-middleware and use the webpack.prod.js
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler));
 
 // Mock API route (just for test)
 app.get("/test", function (_, res) {
@@ -28,7 +39,7 @@ app.post("/sentiment-analysis", async function (req, res) {
 });
 
 // Spin up the server
-const port = 3001;
+const port = 30031;
 app.listen(port, function () {
   console.log(`Listening on port: ${port}`);
 });
---
+++ webpack.prod.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "images/[name][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require.resolve("sass"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      favicon: "./src/client/views/favicon.ico",
+      filename: "./index.html",
+      inject: "body",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
